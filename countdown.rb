b94602707a384ef1a5d7c377d6be84a4fea0afe2
Zach <zachlandis91@gmail.com>
2023-04-07 11:22:41 -0600

    commit tree 87fcba37d65d897885bf8130cbfcc2f96f1eeaef

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,36 @@
-#write your code here
+# #write your code here
 
-def countdown
+# def countdown(int)
+#     while int > 0
+#         puts "#{int} SECOND(S)!"
+#         int -= 1
+#     end
+#     "HAPPY NEW YEAR!"
+# end
+
+
+def countdown(int)
+    while int > 0
+        puts "#{int} SECOND(S)!"
+        int -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+
+# def countdown_with_sleep(int) 
+#     while int > 0
+#         puts "#{int} SECOND(S)!"
+#         sleep(1)
+#         int -= 1
+#     end
+#     "HAPPY NEW YEAR!"
+# end
+
+def countdown_with_sleep(int)
+    while int > 0
+        puts "#{int} SECOND(S)!"
+        sleep(1)
+        int -= 1
+    end
 end
